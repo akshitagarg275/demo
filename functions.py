46df174e8b13a7a957d1933addad652b521368c9
akshitagarg275 <akshitagarg275@gmail.com>
2024-12-13 17:27:35 +0530

    first commit

---
+++ functions.py
@@ -0,0 +1,38 @@
+# print("Hello world")
+
+'''
+Functions -> To write re-usable
+DRY -> Donot Repeat Yourself
+
+syntax:
+
+def function_name(parameters):
+    //function statements
+
+'''
+
+# TODO: Function to print good evening
+def greet():
+    print("Good evening!")
+
+# call the function
+# greet()
+# greet()
+
+# def greet2(name = "NA"):
+#     print("Good evening! ", name)
+
+# greet2("sam")
+# greet2("Rahul")
+# greet2()
+
+
+# TODO: return keyword
+
+def calc(num1, num2):
+    return num1 + num2
+
+
+res = calc(5,6)
+print(res)
+print(calc(1,2))
